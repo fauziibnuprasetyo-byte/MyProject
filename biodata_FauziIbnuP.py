64c8ae9999932fd0a2635b072a61d0d49e0e5427
Fauzi Ibnu Prasetyo <fauziibnuprasetyo@smkitihsanulfikri.sch.id>
2026-01-27 14:26:21 +0700

    Menambahkan  FIle Biodata

---
+++ biodata_FauziIbnuP.py
@@ -0,0 +1,16 @@
+print("=======================================")
+print("Program Biodata Sederhana Dengan Python")
+print("=======================================")
+
+nama = "Fauzi Ibnu Prasetyo"
+hobi = "Masak, Membaca Buku, Traveling"
+alamat = "Bekasi, Jawa Barat"
+kota = "Bekasi"
+
+print("Nama Lengkap  :", nama)
+print("Alamat        :", alamat, "\n                Kota ", kota)
+print("Hobi          :", hobi)
+
+print("=======================================")
+print("      Terima Kasih Telah Mengisi      ")
+print("=======================================")
